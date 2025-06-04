201ba415dbf0c8548ae1e3bc5dbaec3a0c4a80f4
Ikraam26 <ikraamamr@gmail.com>
2025-06-04 11:45:09 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,52 +4,45 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]) {
-    double result = 0.0;
+    if (argc == 3 || argc == 4) {
+        char *op = argv[1];
+        double r = 0;
 
-    if (argc == 4) {
-        // Opérations binaires : add, sub, mul, div
-        const char* op = argv[1];
-        double a = atof(argv[2]);
-        double b = atof(argv[3]);
+        if (strcmp(op, "car") == 0 && argc == 3) {
+            double x = atof(argv[2]);
+            r = _car(x);
+            printf("%lf\n", r);
+        }
+        else if (argc == 4) {
+            char *a = argv[2];
+            char *b = argv[3];
 
-        if (strcmp(op, "add") == 0) {
-            result = _add(a, b);
-        } else if (strcmp(op, "sub") == 0) {
-            result = _sub(a, b);
-        } else if (strcmp(op, "mul") == 0) {
-            result = _mul(a, b);
-        } else if (strcmp(op, "div") == 0) {
-            if (b == 0) {
-                printf("Erreur : division par zéro.\n");
-                return 1;
+            if (strcmp(op, "add") == 0) {
+                r = _add(atof(a), atof(b));
+                printf("%lf\n", r);
             }
-            result = _div(a, b);
-        } else {
-            printf("Erreur : opération binaire inconnue.\n");
-            return 1;
+            else if (strcmp(op, "sub") == 0) {
+                r = _sub(atof(a), atof(b));
+                printf("%lf\n", r);
+            }
+            else if (strcmp(op, "mul") == 0) {
+                r = _mul(atof(a), atof(b));
+                printf("%lf\n", r);
+            }
+            else if (strcmp(op, "div") == 0) {
+                r = _div(atof(a), atof(b));
+                printf("%lf\n", r);
+            }
+            else {
+                printf("Erreur de parametres\n");
+            }
         }
-
-        printf("%lf\n", result);
-        return 0;
-    }
-
-    if (argc == 3) {
-        // Opérations unaires : car
-        const char* op = argv[1];
-        double a = atof(argv[2]);
-
-        if (strcmp(op, "car") == 0) {
-            result = car(a);
-            printf("%lf\n", result);
-            return 0;
-        } else {
-            printf("Erreur : opération unaire inconnue.\n");
-            return 1;
+        else {
+            printf("Erreur de parametres\n");
         }
     }
-
-    // Mauvais nombre d'arguments
-    printf("Usage : ./calculator <operation> <a> [b]\n");
-    printf("Opérations disponibles : add, sub, mul, div, car\n");
-    return 1;
+    else {
+        printf("Erreur de parametres\n");
+    }
+    return 0;
 }
